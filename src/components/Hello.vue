030d14fa358e6fa9d45faca7f6861a304be9dd18
MrDenzzz <admin@mrdenzzz.ru>
2018-05-22 06:21:57 +0300

    Initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="app">
+    <navbar-component />
+    <h1>Start</h1>
+  </div>
+</template>
+
+<script>
+import NavbarComponent from "./modules/NavbarComponent";
+
+export default {
+  name: "hello",
+  components: {
+    NavbarComponent
+  },
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
